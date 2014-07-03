2417d0354da98451ff8bd139860bb2ac94517361
John Hubner <jjh5030@gmail.com>
2014-07-02 20:22:51 -0400

    Lecture 84
    UPDATE CONTACT PAGE

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, RequestContext, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import send_mail
 
 from .models import Contact
 from .forms import ContactForm
@@ -9,8 +10,13 @@ def contact_us(request):
     form = ContactForm(request.POST or None)
     if form.is_valid():
         save_form = form.save(commit=False)
+        send = send_mail("Message from eCommerce",
+                         str(save_form.message),
+                         str(save_form.email),
+                         ['jjh5030@gmail.com'],
+                         fail_silently=True)
         save_form.save()
-
+        return HttpResponseRedirect('/')
 
     return render_to_response('contact/contact_us.html', locals(), context_instance=RequestContext(request))
 
